2c8065d9f4e94e96a38018e4de79807fbeb64f51
Honam Kang <honam.kang@mindfullabs.ai>
2026-05-04 12:02:37 +0900

    Release v0.1.0-alpha.10

--- Casks/mq-dir.rb
+++ Casks/mq-dir.rb
@@ -1,6 +1,6 @@
 cask "mq-dir" do
-  version "0.1.0-alpha.9"
-  sha256 "146ec72fc7f106ca1205ce164426e155fbbdb93a310551f29ce8a8ac0bb50755"
+  version "0.1.0-alpha.10"
+  sha256 "08c9a6a9e3fae83bccab8dcf2849f975ebabbcbba6776efdd8c9ff152a3e797f"
 
   url "https://github.com/h5nam/mq-dir/releases/download/v#{version}/mq-dir-v#{version}.dmg"
   name "mq-dir"
